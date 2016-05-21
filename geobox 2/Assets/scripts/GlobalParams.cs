5f52216f05563a2d8d682bc89fa1af428eff6477
mikho <mikhosaama@gmail.com>
2016-05-21 11:26:44 +0400

    პროექტის ატვირთვა

---
+++ geobox 2/Assets/scripts/GlobalParams.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalParams : MonoBehaviour {
+
+
+	public static int Score;
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}
---
+++ geobox 2/Assets/scripts/gamecontroller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class gamecontroller : MonoBehaviour {
+
+	public float timer;
+
+	public GameObject UnivarsalBall;
+	public List<Sprite> SpriteList;
+	float oldTimer;
+	void Start () {
+		oldTimer = timer;
+	}
+	
+
+	void Update () {
+	
+
+			print(Time.deltaTime);
+			timer-=Time.deltaTime;
+			if(timer<= 0){
+
+			Instantiate(UnivarsalBall, new Vector3(Random.Range(-2f,2f), 5, 0),Quaternion.identity);
+			UnivarsalBall.GetComponent<SpriteRenderer>().sprite =SpriteList[Random.Range (0,SpriteList.Count)];
+			if(oldTimer>0.001f)oldTimer-= 0.001f;
+			timer=oldTimer;
+		}
+
+
+
+
+	}
+}
